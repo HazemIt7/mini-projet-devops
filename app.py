094f7fe8170a35fce1561c0e50c9ac06652bb29f
HazemIt7 <hazem.ayachi@itbs.tn>
2025-04-29 20:03:58 +0100

    modif app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import platform
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     # Affiche "Hello World from..." et le nom d'hôte du conteneur
     hostname = platform.node()
     return f"Hello World from {hostname}!\n"
+
 
 if __name__ == "__main__":
     # Écoute sur toutes les interfaces (0.0.0.0) sur le port 5000
