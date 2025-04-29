fd741e117ee1b67166c125aeb70ebb8b2717f6f8
HazemIt7 <hazem.ayachi@itbs.tn>
2025-04-29 20:08:32 +0100

    modif test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,10 @@
-from app import app # Importer l'instance Flask depuis votre fichier app.py
+from app import app  # Importer l'instance Flask depuis votre fichier app.py
+
 
 def test_hello_route():
-    """Teste si la route '/' retourne un code 200 OK et contient 'Hello World'."""
-    client = app.test_client() # Créer un client de test Flask
-    response = client.get('/') # Faire une requête GET sur '/'
+    """Teste si la route '/' retourne un code 200 OK"""
+    client = app.test_client()  # Créer un client de test Flask
+    response = client.get('/')  # Faire une requête GET sur '/'
 
-    assert response.status_code == 200 # Vérifier que le statut est 200
-    assert b"Hello World" in response.data # Vérifier que la réponse contient le texte attendu (b"" pour bytes)
+    assert response.status_code == 200  # Vérifier que le statut est 200
+    assert b"Hello World" in response.data  # Vérifier que la réponse 
